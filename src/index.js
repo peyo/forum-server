ca65fbc56ebeef6b31d90e9d18c8070d85aff005
peyo <peteryyoon@gmail.com>
2020-12-19 19:45:16 -0800

    Added feed to type Query and new type, Link.

--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ const { ApolloServer } = require("apollo-server");
 const typeDefs = `
   type Query {
     info: String!
+    feed: [Link!]!
+  }
+
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
   }
 `;
 
 const resolvers = {
   Query: {
-    info: () => `This is the API for a forum.`,
+    info: () => null,
   },
 };
 
